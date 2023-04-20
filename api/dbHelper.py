517c71b8ed792c2c824195dd4878b7946ebbce56
Abhay <abhaybro768@gmail.com>
2023-04-20 23:21:43 +0530

    done

---
+++ api/dbHelper.py
@@ -0,0 +1,65 @@
+import psycopg2
+import os
+
+db_str = "postgres://szrufkxz:IVMZS-UXd1LGHFR8I1PzcrsfwPORnXYf@horton.db.elephantsql.com/szrufkxz"
+conn = False #psycopg2.connect(db_str)
+cur = False #conn.cursor()
+
+def closeConn ():
+	global conn
+	global cur
+	cur.close()
+	conn.close()
+	cur = False
+	conn = False
+
+def connect ():
+	global conn
+	global cur
+	conn = psycopg2.connect(db_str)
+	cur = conn.cursor()
+
+def parseOptions (opts, qid):
+	# print(opts)
+	options = []
+	for i in range(len(opts)):
+		option = {'oid' : str(qid)+"."+str(i), 'text' : opts[i]}
+		# print(options, option)
+		options.append(option)
+
+	# print(options)
+	return options;
+
+def getData():
+	if  not conn :
+		connect()
+
+	cur.execute('SELECT * FROM qqs')
+	results = cur.fetchall()
+
+	newResults = []
+	for result in results :
+		qid = result[0]
+		question = result[1]
+
+		option1 = {'oid' : str(qid)+".1" , 'text' : result[2][0] }
+		option2 = {'oid' : str(qid)+".2" , 'text' : result[2][1] }
+		option3 = {'oid' : str(qid)+".3" , 'text' : result[2][2] }
+		option4 = {'oid' : str(qid)+".4" , 'text' : result[2][3] }
+
+		answer = result[3]
+
+		newResult = {
+			'qid' : qid, 
+			'question' : question,
+			'option1' : option1, 
+			'option2' : option2,
+			'option3' : option3,
+			'option4' : option4,
+			'answer' : answer
+		}
+		newResults.append(newResult)
+
+	return newResults
+
+# print(getData())
--- api/index.py
+++ api/index.py
@@ -1,11 +1,17 @@
+from . import dbHelper as db
+
 from flask import Flask
+from markupsafe import escape
+from flask import render_template as rt
+
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return rt('index.html', qs=db.getData())#json.dumps(db.getData())
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/quiz')
+def quiz():
+    return json.dumps(db.getData())
